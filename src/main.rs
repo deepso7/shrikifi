77a58c6fa56bdddf8edbc19a8b4e2197492acfdf
Deepso <deeps.o7o8.y@gmail.com>
2026-05-17 02:38:04 +0530

    led blinking

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
-fn main() {
+use error::Result;
+use esp_idf_hal::delay::FreeRtos;
+use esp_idf_hal::gpio::*;
+use esp_idf_hal::peripherals::Peripherals;
+
+mod error;
+
+fn main() -> Result<()> {
     // It is necessary to call this function once. Otherwise, some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
     esp_idf_svc::sys::link_patches();
@@ -6,9 +13,17 @@ fn main() {
     // Bind the log crate to the ESP Logging facilities
     esp_idf_svc::log::EspLogger::initialize_default();
 
-    loop {
-        log::info!("gggggggggg");
+    let peripherals = Peripherals::take()?;
 
-        std::thread::sleep(std::time::Duration::from_secs(1));
+    let mut led = PinDriver::output(peripherals.pins.gpio21)?;
+
+    loop {
+        log::info!("Turning ON");
+        led.set_high()?; // ON
+        FreeRtos::delay_ms(500);
+
+        log::info!("Turning OFF");
+        led.set_low()?; // OFF
+        FreeRtos::delay_ms(500);
     }
 }
